f3edf028968bfe70ffe99955411fb27db67f1064
dyerkebulan21 <dyerkebulan21@gmail.com>
2021-06-30 17:13:41 +0600

    layout

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# spotify-clone
+Spotify clone create with react/redux/typescript/bootstrap
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+export const Header:React.FC<Props> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
